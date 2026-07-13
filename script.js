5434247a0548457d76e25f0ea023e1adf9691d8a
Solaire1903 <marvkie@gmail.com>
2026-07-13 16:27:54 +0200

    Add checkForWinner function to gameboard

--- script.js
+++ script.js
@@ -14,10 +14,54 @@ const gameController = (() => {
 
         const cellIsEmpty = (row, column) => boardArray[row][column] === '';
 
+        const checkForWinner = () => {
+            let winner;
+
+            //Check rows
+            for (let row = 0; row < 3; ++row) {
+                if (!cellIsEmpty(row, 0)) {
+                    winner = boardArray[row][0];
+                    if (boardArray[row][1] === winner && boardArray[row][2] === winner) {
+                        return winner;
+                    }
+
+                }
+            }
+
+            //Check columns
+            for (let col = 0; col < 3; ++col) {
+                if (!cellIsEmpty(0, col)) {
+                    winner = boardArray[0][col];
+                    if (boardArray[1][col] === winner && boardArray[2][col] === winner) {
+                        return winner;
+                    }
+                }
+            }
+
+            //Check diagonal
+            if (!cellIsEmpty(0, 0)) {
+                winner = boardArray[0][0];
+                if (boardArray[1][1] === winner && boardArray[2][2] === winner) {
+                    return winner;
+                }
+            }
+
+            //Check reverse diagonal
+            if (!cellIsEmpty(0, 2)) {
+                winner = boardArray[0][2];
+                if (boardArray[1][1] === winner && boardArray[2][0] === winner) {
+                    return winner;
+                }
+            }
+
+            return "No winner found";
+        };
+
         return {
             getBoardArray,
             getCell,
-            cellIsEmpty
+            cellIsEmpty,
+            checkForWinner
         };
     })();
 
@@ -48,9 +92,11 @@ const gameController = (() => {
     };
 
     const debugTest = () => {
-        console.log(gameboard.cellIsEmpty(1, 1));
-        player1.placeMark(1, 1);
-        console.log(gameboard.cellIsEmpty(1, 1));
+        player2.placeMark(0, 0);
+        player2.placeMark(1, 1);
+        player2.placeMark(2, 2);
+        printBoard();
+        console.log(gameboard.checkForWinner());
     }
 
     return {
